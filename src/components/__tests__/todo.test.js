a1417e34a0253b45bcdd3d1a68ed758cbde1b12d
mickoangelolacap <mickoangelolacap@gmail.com>
2021-09-23 23:53:24 +0800

    Unit Testing

---
+++ src/components/__tests__/todo.test.js
@@ -0,0 +1,14 @@
+// import { render, screen, cleanup } from '@testing-library/react'
+// import TodoPage from '../ToDoComponents/ToDoPage'
+// import Todo from '../../models/todo'
+
+// afterEach(() => {
+//     cleanup();
+// })
+
+// test('test', () => {
+//     const todo = Todo
+//     render (<TodoPage todo={todo}/>)
+//     const todoElement = screen.getByTestId('test-1')
+//     expect(todoElement).toContain(todo);
+// })
